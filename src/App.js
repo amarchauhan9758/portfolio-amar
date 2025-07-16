dc5f46feb1a0c3103994bd79c6739ebb4cec429f
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-07-16 21:16:07 +0530

    coode commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Home />
     </div>
   );
 }
