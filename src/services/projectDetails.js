61c6eaaffbfad2ea69f86563897dfe993972e77d
amarchauhan9758 <aryanchauhan9758@gmail.com>
2025-08-18 00:01:33 +0530

    code commit

--- src/services/projectDetails.js
+++ src/services/projectDetails.js
@@ -1,62 +1,78 @@
 // data/projects.js
 export const projects = [
-    {
-        title: "Job Portal",
-        description: "A modern job portal connecting skilled professionals with top employers across industries.",
-        image: "https://media.licdn.com/dms/image/v2/D5612AQHmWpsKqCEUgQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1692110377639?e=2147483647&v=beta&t=8o8pbq1bZtrQjOZxyCis8GkK8FtHx38WqMoea3qjPB0",
-        link: "https://gentle-semifreddo-134a1f.netlify.app/login",
-    },
-    {
-        title: "Fazer.io",
-        description: "A community-powered decentralized exchange (DEX) built on Polygon, combining the power of DeFi and NFTs with a strong focus on autonomy, anonymity, and security.",
-        image: "https://fazer-lr3fmvje8-amarchauhan9758.vercel.app/_next/static/media/Box.03783472.png",
-        link: "https://fazer-lr3fmvje8-amarchauhan9758.vercel.app/",
-    },
-    {
-        title: "Memo Swap",
-        description: "MMO Swap is a next-generation Decentralized Exchange (DEX) built on the Polygon Network, designed specifically to support and empower the Web3 gaming ecosystem",
-        image: "https://melodious-babka-49c9ac.netlify.app/_next/static/media/HeroLogoText.504ea7d5.png?imwidth=750",
-        link: "https://melodious-babka-49c9ac.netlify.app/",
-    },
-    {
-        title: "Lazarus Pools",
-        description: "A user-friendly DeFi staking platform (called Lazarus Pools) made for safe, simple crypto earning, especially for beginners, built in partnership with TechTree.",
-        image: "https://wizard-token.gitbook.io/lazarus-pool/~gitbook/image?url=https%3A%2F%2F1109902788-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252Fru8r4eR83VUqdzHl9f7B%252Fuploads%252FDgOUM78lhIJjmxezYUgQ%252FLazarus%2520Pool%2520Winter.png%3Falt%3Dmedia%26token%3Dc86fd49f-a3bf-4c84-ac3a-4b15632d8f24&width=1248&dpr=4&quality=100&sign=cf06c5aa&sv=2",
-        link: "https://papaya-lazarus-pools.netlify.app/#/bsc",
-    },
-    {
-        title: "Tailinwind CSS Clone",
-        description: "Cone of Tailwind CSS website.",
-        image: "https://deft-chimera-bb35b6.netlify.app/assets/wysbBMS.0f46e3b0.png",
-        link: "https://deft-chimera-bb35b6.netlify.app/",
-    },
+  {
+    title: "DevTinder Plateform",
+    description:
+      "A social platform connecting developers to network, send friend requests, and chat in real-time using Socket.IO. Integrated premium subscription payments with Razorpay for enhanced features.",
+    image:
+      "https://media.licdn.com/dms/image/v2/D5612AQHmWpsKqCEUgQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1692110377639?e=2147483647&v=beta&t=8o8pbq1bZtrQjOZxyCis8GkK8FtHx38WqMoea3qjPB0",
+    link: "https://devtinder-07.netlify.app/",
+  },
+  {
+    title: "Job Portal",
+    description:
+      "A modern job portal connecting skilled professionals with top employers across industries.",
+    image:
+      "https://media.licdn.com/dms/image/v2/D5612AQHmWpsKqCEUgQ/article-cover_image-shrink_600_2000/article-cover_image-shrink_600_2000/0/1692110377639?e=2147483647&v=beta&t=8o8pbq1bZtrQjOZxyCis8GkK8FtHx38WqMoea3qjPB0",
+    link: "https://gentle-semifreddo-134a1f.netlify.app/login",
+  },
+  {
+    title: "Fazer.io",
+    description:
+      "A community-powered decentralized exchange (DEX) built on Polygon, combining the power of DeFi and NFTs with a strong focus on autonomy, anonymity, and security.",
+    image:
+      "https://fazer-lr3fmvje8-amarchauhan9758.vercel.app/_next/static/media/Box.03783472.png",
+    link: "https://fazer-lr3fmvje8-amarchauhan9758.vercel.app/",
+  },
+  {
+    title: "Memo Swap",
+    description:
+      "MMO Swap is a next-generation Decentralized Exchange (DEX) built on the Polygon Network, designed specifically to support and empower the Web3 gaming ecosystem",
+    image:
+      "https://melodious-babka-49c9ac.netlify.app/_next/static/media/HeroLogoText.504ea7d5.png?imwidth=750",
+    link: "https://melodious-babka-49c9ac.netlify.app/",
+  },
+  {
+    title: "Lazarus Pools",
+    description:
+      "A user-friendly DeFi staking platform (called Lazarus Pools) made for safe, simple crypto earning, especially for beginners, built in partnership with TechTree.",
+    image:
+      "https://wizard-token.gitbook.io/lazarus-pool/~gitbook/image?url=https%3A%2F%2F1109902788-files.gitbook.io%2F%7E%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252Fru8r4eR83VUqdzHl9f7B%252Fuploads%252FDgOUM78lhIJjmxezYUgQ%252FLazarus%2520Pool%2520Winter.png%3Falt%3Dmedia%26token%3Dc86fd49f-a3bf-4c84-ac3a-4b15632d8f24&width=1248&dpr=4&quality=100&sign=cf06c5aa&sv=2",
+    link: "https://papaya-lazarus-pools.netlify.app/#/bsc",
+  },
+  {
+    title: "Tailinwind CSS Clone",
+    description: "Cone of Tailwind CSS website.",
+    image:
+      "https://deft-chimera-bb35b6.netlify.app/assets/wysbBMS.0f46e3b0.png",
+    link: "https://deft-chimera-bb35b6.netlify.app/",
+  },
 
+  {
+    title: "Candy Hunt",
+    description:
+      "Candy Hunt is a fun and exciting game of chance and speed powered by Vegas Finance.",
+    image:
+      "https://bejewelled-lily-ee8706.netlify.app/static/media/lootbox.4b51f2f126fa6aabb1c4.png",
 
-    {
+    link: "https://bejewelled-lily-ee8706.netlify.app/",
+  },
 
-        title: "Candy Hunt",
-        description: "Candy Hunt is a fun and exciting game of chance and speed powered by Vegas Finance.",
-        image: "https://bejewelled-lily-ee8706.netlify.app/static/media/lootbox.4b51f2f126fa6aabb1c4.png",
+  {
+    title: "Mooky io",
+    description:
+      "MOOKY is a Web3-powered environmental initiative that combines NFTs and blockchain with real-world tree planting.",
+    image:
+      "https://endearing-bombolone-5d3674.netlify.app/assets/images/taidy.svg",
 
-        link: 'https://bejewelled-lily-ee8706.netlify.app/'
-    }
+    link: "https://endearing-bombolone-5d3674.netlify.app/",
+  },
 
-
-    ,
-    {
-        title: "Mooky io",
-        description: "MOOKY is a Web3-powered environmental initiative that combines NFTs and blockchain with real-world tree planting.",
-        image: 'https://endearing-bombolone-5d3674.netlify.app/assets/images/taidy.svg',
-
-        link: "https://endearing-bombolone-5d3674.netlify.app/",
-
-    },
-
-    {
-        title: "Colleage website",
-        description: "College website made with React and Tailwind css.",
-        image: "https://ui-assignment-ten.vercel.app/static/media/Mask%20Group%2053.de7be23244270cdc52f6.png",
-        link: "https://ui-assignment-ten.vercel.app/",
-    },
-
+  {
+    title: "Colleage website",
+    description: "College website made with React and Tailwind css.",
+    image:
+      "https://ui-assignment-ten.vercel.app/static/media/Mask%20Group%2053.de7be23244270cdc52f6.png",
+    link: "https://ui-assignment-ten.vercel.app/",
+  },
 ];
